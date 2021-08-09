2d49188f91727861e93e5c1dfb09f76dfd2340bb
Juliano Vince De Campos <juliano.campos@mercadolivre.com>
2021-08-09 20:17:20 -0300

    Initial Commit

---
+++ CoronaStatus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoronaStatus
+//
+//  Created by Juliano Vince De Campos on 09/08/21.
+//  Copyright © 2021 Juliano Vince De Campos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
