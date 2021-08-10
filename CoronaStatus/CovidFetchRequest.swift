57c8b222edcb6bb5a354b9cb94dec68ec5fb68b3
Juliano Vince De Campos <juliano.campos@mercadolivre.com>
2021-08-10 09:38:12 -0300

    Criação, configuração de projeto que verifica com status do covid-19 no mundo. Já implementei as classes model, CovidFetchRequest

---
+++ CoronaStatus/CovidFetchRequest.swift
@@ -0,0 +1,45 @@
+//
+//  CovidFetch.swift
+//  CoronaStatus
+//
+//  Created by Juliano Vince De Campos on 09/08/21.
+//  Copyright © 2021 Juliano Vince De Campos. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class CovidFetchRequest: ObservableObject {
+    
+    @Published var totalData: TotalData = 
+   
+    init() {
+        getCurrentTotal()
+    }
+    
+    func getCurrentTotal(){
+        
+        let headers: HTTPHeaders = [
+            "x-rapidapi-key": "2a1af1aa1amshd3e607321375ee7p191601jsnddba03e9cdc2",
+            "x-rapidapi-host": "covid-19-data.p.rapidapi.com"
+        ]
+    
+        AF.request("https://covid-19-data.p.rapidapi.com/totals?format=undefined", headers: headers).responseJSON { response in
+            
+            let result = response.data
+            
+            if result != nil {
+                let json = JSON(result!)
+               
+                let confirmed = json[0]["recovered"].intValue
+                let deaths = json[0]["deaths"].intValue
+                let recovered = json[0]["recovered"].intValue
+                let critical = json[0]["critical"].intValue
+           
+                TotalData(confirmed: confirmed, critical: critical, deaths: deaths, recovered: recovered)
+            }
+        }
+        
+    }
+}
---
+++ CoronaStatus/Model.swift
@@ -0,0 +1,47 @@
+//
+//  File.swift
+//  CoronaStatus
+//
+//  Created by Juliano Vince De Campos on 09/08/21.
+//  Copyright © 2021 Juliano Vince De Campos. All rights reserved.
+//
+
+import Foundation
+
+struct TotalData {
+    
+    let confirmed: Int
+    let critical: Int
+    let deaths: Int
+    let recovered: Int
+    
+    var fatalityRat: Double {
+        return (100.00 * Double(deaths)) / Double(confirmed)
+    }
+    
+    var recoveredRate: Double {
+        return (100.00 * Double(recovered)) / Double(confirmed)
+    }
+}
+
+
+struct CoutryData {
+    
+    let country:String
+    let confirmed: Int64
+    let critical: Int64
+    let deaths: Int64
+    let recovered: Int64
+    let latitude: Double
+    let longitude: Double
+    
+    var fatalityRat: Double {
+        return (100.00 * Double(deaths)) / Double(confirmed)
+    }
+    
+    var recoveredRate: Double {
+        return (100.00 * Double(recovered)) / Double(confirmed)
+    }
+}
+
+let testTotalData = TotalData(confirmed: 200, critical: 100, deaths: 20, recovered: 50)
