1307fd9275523eb7bd50c86f51067bd66625fc42
Juliano Vince De Campos <juliano.campos@mercadolivre.com>
2021-08-10 16:43:59 -0300

    Organização de arquivos, criação de classe responsavel por layout fixo

---
+++ CoronaStatus/ListHeaderView.swift
@@ -0,0 +1,53 @@
+//
+//  ListHeaderView.swift
+//  CoronaStatus
+//
+//  Created by Juliano Vince De Campos on 10/08/21.
+//  Copyright © 2021 Juliano Vince De Campos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ListHeaderView: View {
+    var body: some View {
+        HStack {
+            
+            Text("Country")
+                .fontWeight(.bold)
+                .font(.subheadline)
+                .frame(width: 110,alignment: .leading)
+                .padding(.leading, 15)
+            
+            Spacer()
+            
+            Text("Conf.")
+            .fontWeight(.bold)
+            .font(.subheadline)
+            .frame(width: 40)
+            .padding(.leading, 5)
+            
+            Spacer()
+            
+            Text("Death")
+            .fontWeight(.bold)
+            .font(.subheadline)
+            .frame(height: 40)
+            .padding(.leading, 5)
+            
+            Spacer()
+            
+            Text("Recover")
+            .fontWeight(.bold)
+            .font(.subheadline)
+            .frame(height: 40)
+            .padding(.trailing, 15)
+        }
+        .background(Color.gray)
+    }
+}
+
+struct ListHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListHeaderView()
+    }
+}
